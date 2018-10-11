1ac06c052683d0a68e1a4111f3fab2a87165c352
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-10-12 01:31:14 +0900

    fove with position

---
+++ Assets/LightAdjust.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightAdjust : MonoBehaviour {
+	public Light light;
+	bool lightEnable;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			lightEnable = !lightEnable;
+			light.enabled = lightEnable;
+		}
+	}
+}
---
+++ Assets/PositionAdjust.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionAdjust : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vert = Input.GetAxis("Vertical");
+		gameObject.transform.Translate(Vector3.back * vert * Time.deltaTime);
+	}
+}
